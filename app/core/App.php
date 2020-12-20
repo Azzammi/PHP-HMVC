bd4bfd774cd9c79e810dff0371326a362efa1dac
Luthfi Azzammi <luthfia911@gmail.com>
2020-12-20 08:56:23 +0700

    Part 3 : Routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,19 @@
 class App{
     public  function  __construct()
     {
-        echo "Terpanggil !";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL(){
+        if( isset($_GET['url'])){
+            //Remove Slash '/'
+            $url = rtrim($_GET['url'],'/');
+            //Bersihkan dar karakter ngaco
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //Pecah url
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
