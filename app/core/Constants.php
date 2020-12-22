e71016411f455b14ae8a41714aa2ee8433c66ac4
Luthfi Azzammi <luthfia911@gmail.com>
2020-12-22 10:42:07 +0700

    Part 6 : Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+//Absolute Url
+define('BASEURL','http://localhost:81/phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,7 @@
-<header>
-    <h1>About Index</h1>
-</header>
-<p>Hai, Nama Saya <?= $data['nama'] ?>, Umur saya <?= $data['umur'] ?>, dan Pekerjaan saya <?= $data['pekerjaan'] ?></p>
+<div class="container">
+    <header class="mt-4">
+        <h1>About Index</h1>
+    </header>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" width="200" alt="Muhammad Luthfi Azzammi" class="rounded-circle shadow">
+    <p>Hai, Nama Saya <?= $data['nama'] ?>, Umur saya <?= $data['umur'] ?>, dan Pekerjaan saya <?= $data['pekerjaan'] ?></p>
+</div>
